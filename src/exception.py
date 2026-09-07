14939edbf79977ede0c5a215856ebb1896586e06
abhed8604 <abhed10601@gmail.com>
2026-09-07 20:06:57 +0530

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -9,7 +9,7 @@ def error_message_detail(error,error_detail:sys):
     return error_message
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self,error_message,error_detail:sys): # here we get error message(e) and error_detail(sys) from the error 
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
