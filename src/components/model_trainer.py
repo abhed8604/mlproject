50d6ba522f3b4ce50314886cc1f9520ad74b30da
abhed8604 <abhed10601@gmail.com>
2026-09-10 07:01:43 +0530

    hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -48,8 +48,48 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
+
+            # added all params for all models for hyperparameter tuning
+            params = {
+                "Decision Tree": {
+                    "max_depth": [3, 5, 7, 10, None],
+                    "min_samples_split": [2, 5, 10],
+                },
+                "Random Forest": {
+                    "n_estimators": [50, 100, 200],
+                    "max_depth": [None, 5, 10],
+                    "max_features": ["sqrt", None],
+                },
+                "Gradient Boosting": {
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "n_estimators": [100, 200],
+                    "max_depth": [3, 5],
+                    "subsample": [0.8, 1.0],
+                },
+                "Linear Regression": {},
+                "XGBRegressor": {
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "n_estimators": [100, 200],
+                    "max_depth": [3, 5, 7],
+                },
+                "CatBoosting Regressor": {
+                    "depth": [4, 6, 8],
+                    "learning_rate": [0.03, 0.1],
+                    "iterations": [100, 200],
+                },
+                "AdaBoost Regressor": {
+                    "learning_rate": [0.01, 0.1, 1.0],
+                    "n_estimators": [50, 100, 200],
+                },
+                "K-Neighbors Regressor": {
+                    "n_neighbors": [3, 5, 7, 9, 11],
+                    "weights": ["uniform", "distance"],
+                    "p": [1, 2],
+                }
+            }
+            
             # we create a report for all the models performance
-            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            model_report:dict=evaluate_model(X_train,y_train,X_test,y_test,models,params)
 
             # now we will get the best model for our needs
             best_model_score=max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,10 @@ import os
 import sys
 import numpy as np
 import pandas as pd
-
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -21,16 +21,25 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_model(X_train,y_train,X_test,y_test,models): # we are creating an function which will evaluate all the models in models dict for our data
+def evaluate_model(X_train,y_train,X_test,y_test,models,params): # we are creating an function which will evaluate all the models in models dict for our data
     try:
         report={} # we made our report dict to store our result
 
         for i in range(len(list(models))):
             model=list(models.values())[i] # here we select one model
+            para=params[list(models.keys())[i]]
 
-            model.fit(X_train,y_train)
+            gs = GridSearchCV( # doing grid search for hyperparameter tuning
+                estimator=model,
+                param_grid=para,
+                cv=3,
+            )
+            gs.fit(X_train,y_train)
+            
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train) # we are fitting the best model we found from grid search CV
 
-            # we get out predicted values for our test array and then get r2 score for it 
+            # we get our predicted values for our test array and then get r2 score for it 
             y_test_pred=model.predict(X_test)
             test_model_score=r2_score(y_test,y_test_pred)
 
